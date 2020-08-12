76887c21509c9f026f84caeea26b365e1316c253
Maung Naing <maungnaing1337@gmail.com>
2020-08-11 19:14:42 -0700

    unable to get the real ip address
    probably will have to parse it some more

--- server.js
+++ server.js
@@ -24,7 +24,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res)=>{
+  console.log(req.headers);
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(ip)
+  res.json({ip_address:ip,"language":req.headers["accept-language"],"software":req.headers["user-agent"]})
+})
+//{"ipaddress":"159.20.14.100",
+//"language":"en-US,en;q=0.5",
+//"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
 
+//
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
